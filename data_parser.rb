acad25c6a3b7660993dbd3077fa95b9a033a8231
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-02-07 16:02:48 -0500

    explorer

--- data_parser.rb
+++ data_parser.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Delivery
   attr_accessor :planet,
                 :shipment,
@@ -8,7 +10,7 @@ class Delivery
 
   def initialize(destination:, what_got_shipped:, number_of_crates:, money_we_made:)
       @planet = destination.downcase
-      @shipment = what_got_shipped
+      self.shipment = what_got_shipped
       @crates = number_of_crates.to_f
       @revenue = money_we_made.to_f
       @bonus = revenue * 0.1
@@ -21,29 +23,19 @@ class Delivery
     when 'mars' then 'Amy'
     when 'uranus' then 'Bender'
     else 'Leela'
+    end
   end
-
-
 end
 
-require 'csv'
-deliveries = CSV.foreach("planet_express_logs.csv", headers: true, header_converters: :symbol)
-end
+deliveries = CSV.foreach("planet_express_logs.csv", headers: true, header_converters: :symbol).collect {|row| Delivery.new(row)}
 
-new_delivery = []
-
-data.each {|row| new_delivery << Delivery.new(row)}
+sum = deliveries.inject(0) {|sum, data| sum + data.revenue}
+puts sum
 
 pilot = "Fry"
 
-sum = new_delivery.inject(0) {|sum, data| sum + deliveries.revenue}
-puts sum
+deliveries.group_by(&:pilot).each {|pilot, deliveries|}
 
-deliveries.group_by(&:pilot).each do |pilot, deliveries|
-
-puts deliveries.select{|delivery| deliveries.pilot == pilot}.inject(0) {|sum, data| sum + data.bonus}
-
-puts "#{pilot} is owed #{deliveries.inject(0) {|sum, deliveries|} sum + deliveries.bonus} bonus"
+puts deliveries.select{|deliveries| deliveries.find_pilot == pilot}.inject(0) {|sum, data| sum + data.bonus}
 
 puts "#{pilot} made #{deliveries.count} deliveries"
-end
