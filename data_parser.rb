9f3fe1ccb1b9d031895d920f9058750e5c33961e
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-02-25 14:15:43 -0500

    changing outputs and finishing bonus addition

--- data_parser.rb
+++ data_parser.rb
@@ -11,8 +11,8 @@ class Delivery
   def initialize(destination:, what_got_shipped:, number_of_crates:, money_we_made:)
     @planet = destination.downcase
     @shipment = what_got_shipped
-    @crates = number_of_crates.to_f
-    @revenue = money_we_made.to_f
+    @crates = number_of_crates.to_i
+    @revenue = money_we_made.to_i
     @bonus = revenue * 0.1
     @pilot = find_pilot
   end
@@ -29,13 +29,12 @@ end
 
 deliveries = CSV.foreach('planet_express_logs.csv', headers: true, header_converters: :symbol).collect { |row| Delivery.new(row) }
 
-sum = deliveries.inject(0) { |sum, data| sum + data.revenue }
-puts sum
+puts "We made #{deliveries.inject(0) { |sum, data| sum + data.revenue }} this week"
 
-pilot = 'Fry'
-
-deliveries.group_by(&:pilot).each { |pilot, deliveries| }
 
 puts deliveries.select { |deliveries| deliveries.find_pilot == pilot }.inject(0) { |sum, data| sum + data.bonus }
 
 puts "#{pilot} made #{deliveries.count} deliveries"
+
+deliveries.group_by(&:pilot).each do |pilot, deliveries|
+end
